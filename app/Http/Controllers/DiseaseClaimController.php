e0320cb179d1273f964bba121a5ad0bc723bf2e1
Abdul5050 <tacticshustle@gmail.com>
2023-11-29 10:54:17 +0100

    update on disease claims

--- app/Http/Controllers/DiseaseClaimController.php
+++ app/Http/Controllers/DiseaseClaimController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreDiseaseClaimRequest;
 use App\Http\Requests\UpdateDiseaseClaimRequest;
+use App\Models\Employer;
 use App\Models\DiseaseClaim;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\DiseaseClaimEmail;
+use Illuminate\Support\Facades\DB;
 
 class DiseaseClaimController extends Controller
 {
@@ -35,9 +39,53 @@ class DiseaseClaimController extends Controller
         $validated['document'] = request()->file('document')->store('claims_documents', 'public');
         $validated['branch_id'] = auth()->user()->branch->id;
 
-        auth()->user()->disease_claims()->create($validated);
+        $diseaseClaim = auth()->user()->disease_claims()->create($validated);
+        try {
+            // Send message to notify specific users based on roles
+            $user = auth()->user();
+            $userBranchId = $user->branch->id;
+        
+            // Fetch all the email addresses with the same branch_id
+            $employerEmailAddresses = Employer::where('branch_id', $userBranchId)
+                ->pluck('company_email');
+        
+            // Fetch user roles and email from the database using Spatie Roles
+            $userDetails = DB::table('model_has_roles')
+                ->join('roles', 'model_has_roles.role_id', '=', 'roles.id')
+                ->join('users', 'model_has_roles.model_id', '=', 'users.id')
+                ->where('model_has_roles.model_type', 'App\Models\User') // Adjust the model type if necessary
+                //->where('users.id', $user->id)
+                ->select('users.email', 'roles.name as role')
+                ->first();
+        
+            // Define the roles you want to notify
+            $targetRoles = ['super-admin', 'MD', 'HOD', 'Regional Manager', 'CERTIFICATE/COMPLIANCE', 'MER', 'ED FINANCE & ACCOUNT', 'AUDIT', 'GM'];
+        
+            // Filter email addresses based on user roles
+            $filteredEmailAddresses = [];
+        
+            if (!empty(array_intersect([$userDetails->role], $targetRoles))) {
+                // If the user has any of the target roles, include their email
+                $filteredEmailAddresses[] = $userDetails->email;
+            }
+        
+            // Include employer email addresses
+            $filteredEmailAddresses = array_merge($filteredEmailAddresses, $employerEmailAddresses->toArray());
+        
+            // Remove duplicates
+            $filteredEmailAddresses = array_unique($filteredEmailAddresses);
+        
+            // Send thank you email to each filtered email address
+            foreach ($filteredEmailAddresses as $email) {
+                Mail::to($email)->send(new DiseaseClaimEmail($diseaseClaim));
+            }
+        
+            return redirect()->route('disease.index')->with('success', 'Disease claim created successfully!');
+        } catch (\Exception $e) {
+            // Handle the exception here
+            return redirect()->route('disease.index')->with('error', 'Failed to notify everyone: ' . $e->getMessage());
+        }
 
-        return redirect()->route('death.index')->with('success', 'Disease claim created successfully!');
     }
 
     /**
